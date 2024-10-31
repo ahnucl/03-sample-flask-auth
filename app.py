55772a0573d786ab8f8ef831c51140da868e5982
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2024-10-31 10:32:38 -0300

    Remove espaços

--- app.py
+++ app.py
@@ -32,21 +32,21 @@ def login():
     data = request.json
     if not data:
         return jsonify({"message": "Credenciais inválidas"}), 400
-        
+
     username = data.get('username')
     password = data.get('password')
-    
+
     if not username and not password:
         return jsonify({"message": "Credenciais inválidas"}), 400
-    
+
     user = User.query.filter_by(username=username).first()
-    
+
     if not user or user.password != password:
         return jsonify({"message": "Credenciais inválidas"}), 400
-    
+
     login_user(user)
     print(current_user.is_authenticated)
-    
+
     return jsonify({"message": "Autenticação realizada com sucesso"})
 
 
@@ -55,30 +55,30 @@ def create_user():
     data = request.json
     if not data:
         return jsonify({"message": "Dados inválidos"}), 400
-    
+
     username = data.get("username")
     password = data.get("password")
 
     if not username or not password:
         return jsonify({"message": "Dados inválidos"}), 400
-    
+
     user = User(username=username, password=password) # type: ignore
     db.session.add(user)
     db.session.commit()
 
-    
+
     return jsonify({"message": "Usuário cadastrado com sucesso"}), 200
-    
+
 
 @app.route('/users/<int:user_id>', methods=['GET'])
 @login_required
 def read_user(user_id):
     user = User.query.get(user_id)
-    
+
     if not user:
         return jsonify({"message": "Usuário não encontrado"}), 404
-    
-    
+
+
     return jsonify({"username": user.username})
 
 
@@ -86,17 +86,17 @@ def read_user(user_id):
 @login_required
 def update_user(user_id):
     user = User.query.get(user_id)
-    
+
     if not user:
         return jsonify({"message": "Usuário não encontrado"}), 404
-    
+
     data = request.json
     if not data:
         return jsonify({"message": "Dados inválidos"}), 400
-    
+
     user.password = data.get("password")
     db.session.commit()
-    
+
     return jsonify({"message": f"Usuário {user_id} atualizado com sucesso"})
 
 
@@ -105,15 +105,15 @@ def update_user(user_id):
 def delete_user(user_id):
     if user_id == current_user.id:
         return jsonify({"message": "Não é possível deletar o usuário logado"}), 403
-    
+
     user = User.query.get(user_id)
-    
+
     if not user:
         return jsonify({"message": "Usuário não encontrado"}), 404
-    
+
     db.session.delete(user)
     db.session.commit()
-    
+
     return jsonify({"message": f"Usuário {user_id} deletado com sucesso"})
 
 
